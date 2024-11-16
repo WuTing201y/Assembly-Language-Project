c51acd0f8968532ff87e4d4f1c34d490745d0433
WuTing201y <wuting.work@gmail.com>
2024-11-16 16:02:22 +0800

    Update Whack-a-Mole.cpp

--- Whack-a-Mole.cpp
+++ Whack-a-Mole.cpp
@@ -11,33 +11,51 @@ void generateMole(int grid[GRID_SIZE][GRID_SIZE]);
 int playerTurn(int grid[GRID_SIZE][GRID_SIZE]);
 
 int main() {
-    int grid[GRID_SIZE][GRID_SIZE] = {0}; // 初始化地圖為空
-    int score = 0; // 玩家得分
-    int rounds = 0; // 已進行回合數
+    int playAgain = 1; // 控制是否繼續遊戲
 
     srand(time(NULL)); // 初始化隨機數生成器
 
     printf("歡迎來到打地鼠遊戲！\n");
     printf("地圖大小為 %dx%d，每回合隨機出現一隻地鼠。\n", GRID_SIZE, GRID_SIZE);
 
-    while (rounds < MAX_ROUNDS) {
-        printf("\n第 %d 回合：\n", rounds + 1);
+    while (playAgain) {
+        int grid[GRID_SIZE][GRID_SIZE] = {0}; // 初始化地圖為空
+        int score = 0; // 玩家得分
+        int rounds = 0; // 已進行回合數
 
-        // 產生地鼠
-        generateMole(grid);
+        time_t startTime, endTime; // 宣告用於計時的變數
 
-        // 顯示地圖
-        printGrid(grid);
+        time(&startTime); // 記錄遊戲開始時間
 
-        // 玩家行動
-        score += playerTurn(grid);
+        // 遊戲主迴圈
+        while (rounds < MAX_ROUNDS) {
+            printf("\n第 %d 回合：\n", rounds + 1);
 
-        // 回合數 +1
-        rounds++;
+            // 產生地鼠
+            generateMole(grid);
+
+            // 顯示地圖
+            printGrid(grid);
+
+            // 玩家行動
+            score += playerTurn(grid);
+
+            // 回合數 +1
+            rounds++;
+        }
+
+        time(&endTime); // 記錄遊戲結束時間
+        double elapsedTime = difftime(endTime, startTime); // 計算花費的時間
+
+        printf("\n遊戲結束！您的總得分是：%d/%d\n", score, MAX_ROUNDS);
+        printf("您完成此局花費的時間為 %.2f 秒。\n", elapsedTime);
+
+        // 問玩家是否再來一局
+        printf("是否要再來一局？(1: 是, 0: 否)：");
+        scanf("%d", &playAgain);
     }
 
-    printf("\n遊戲結束！您的總得分是：%d/%d\n", score, MAX_ROUNDS);
-    printf("感謝遊玩！\n");
+    printf("感謝遊玩！再見！\n");
     return 0;
 }
 
