085b4afc088f9f7a330d19deed1756cee9f9dcbf
WuTing201y <wuting.work@gmail.com>
2024-11-26 14:33:31 +0800

    Update 1A2B.cpp

--- 1A2B.cpp
+++ 1A2B.cpp
@@ -3,66 +3,66 @@
 #include<stdlib.h>
 #include<time.h>
 #include<conio.h>
-int u; //全局变量
-int same(int num[]);//函数声明
-void menu()//函数声明
+int u; //全區變量
+int same(int num[]);//函數聲明
+void menu()//函數聲明
 {
-	printf("\t\t\t\t猜数字游戏\n\n\n\n");
-	printf("\t\t\t\t1.开始游戏\n\n");
-	printf("\t\t\t\t2.帮助\n\n");
+	printf("\t\t\t\t猜數字遊戲\n\n\n\n");
+	printf("\t\t\t\t1.開始遊戲\n\n");
+	printf("\t\t\t\t2.幫助\n\n");
 	printf("\t\t\t\t3.退出\n\n");
-	printf("\t\t\t请输入（1-3）：");
+	printf("\t\t\t請輸入（1-3）：");
 }
-//主函数
+//主函數
 int main()
 {
 	void help();//函数声明
-	void game(int a[]);//函数声明
+	void game(int a[]);//函數聲明
  
 	int  k = 0, select=0;
 	int a[4] = { 0 };
-	srand((unsigned)time(NULL));//随机数种，只调用一次即可
+	srand((unsigned)time(NULL));//隨機數種，只調用一次即可
 	menu();
 	scanf("%d", &select);
 	while (select!= 3)
 	{
 		switch (select)
 		{
-		case 1: {system("cls"); game(a); break; }
-		case 2: {system("cls"); help(); _getch(); system("cls"); menu(); break; }
-		default:printf("输入不合法，请重新输入!"); break;
+		case 1: {system("cls"); game(a); break; } //開始遊戲
+		case 2: {system("cls"); help(); _getch(); system("cls"); menu(); break; } //幫助
+		default:printf("輸入不合法，請重新輸入!"); break; //輸入1.2.以外的數
 		}
 		scanf("%d", &select);
 	}
 	return 0;
 }
  
-void help()//游戏规则说明函数
+void help()//遊戲規說明函數
 {
-	printf("\t\t\t欢迎使用本程序\n\n\n");
-	printf("游戏说明：输入四位数字，输入后有提示XAYB，X表示有几个数字与答案数字相同且位置相同。\n");
-	printf("Y表示有几位数字与答案数字相同且位置不正确。\n");
-	printf("按任意键继续............\n");
+	printf("\t\t\t歡迎使用本程序\n\n\n");
+	printf("遊戲說明：輸入四位數字，輸入後有提示XAYB，X表示有幾個數字與答案數字相同且位置相同。\n");
+	printf("Y表示有幾位數字與答案數字相同但位置不正確。\n");
+	printf("按任意鍵繼續............\n");
 }
  
-void game(int a[])//比较用户输入数据和随机数，并产生提示信息
+void game(int a[])//比較用戶輸入數四和隨機數，並產生提示訊息
 {
 	int b[4] = {0};
 	int i = 0, j = 0;
 	while (1)
 	{
-		//生成4个随机数
+		//生成4個隨機數字
 		do
 		{
 			for (i = 0; i < 4; i++)
 				a[i] = rand() % 10;
 			same(a);
-		} while (u);//此处用到全局变量u
+		} while (u);//此處使用全局變量u
 		if (a[0] != 0)
 			break;
 	}
 	int k = 1, A = 0, B = 0, N = 8 ,pick = 0;
-	printf("请输入要猜想的四位数\n");
+	printf("請輸入猜想的四位數\n");
 	while (A != 4 && k <= N)
 	{
 		A = 0, B = 0;
