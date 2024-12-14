0931ce601efc4f15280c5e12186d8775eff92ddc
WuTing201y <wuting.work@gmail.com>
2024-12-14 20:59:24 +0800

    Update eng_1A2B.cpp

--- eng_1A2B.cpp
+++ eng_1A2B.cpp
@@ -1,161 +1,126 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<conio.h>
 
 int u; // 全區變量
-
+void creatNUM(int randNum[]);
 int same(int num[]); // 函數聲明
-void record_time(time_t start_time, time_t end_time); // 記錄時間函數聲明
-
-void menu() // 函數聲明
-{
-    printf("Guess Number Game\n\n");
-    printf("1. Start Game\n\n");
-    printf("2. Help\n\n");
-    printf("3. Exit\n\n");
-    printf("Please enter (1-3): ");
-}
+void progress_bar(int current, int total);
 
 // 主函數
 int main()
 {
+    void menu();
     void help(); // 函數聲明
-    void game(int a[]); // 函數聲明
+    void game(int randNum[]); // 函數聲明
 
-    int k = 0, select = 0;
-    int a[4] = { 0 };
+    int select = 0;
+    int randNum[4] = { 0 };
     srand((unsigned)time(NULL)); // 隨機數種，只調用一次即可
     menu();
     scanf("%d", &select);
     while (select != 3)
-    {
-        switch (select)
-        {
-        case 1: { system("cls"); game(a); break; } // 開始遊戲
-        case 2: { system("cls"); help(); _getch(); system("cls"); menu(); break; } // 幫助
-        default: printf("Invalid input, please re-enter!"); break; // 輸入1.2.以外的數
+    {   switch (select){
+        case 1: { game(randNum); break; } // 開始遊戲
+        case 2: { help(); break; } // 幫助
+        case 3: { break;} //退出遊戲
+        default: printf("Invalid input! Please enter (1-3): "); break; // 輸入1.2.以外的數
         }
-        scanf("%d", &select);
+        if (select != 3){
+            scanf("%d", &select);
+        }
+        else break;
     }
     return 0;
+}
+void menu() // 函數聲明
+{
+    printf("Guess Number Game\n");
+    printf("1. Start Game\n");
+    printf("2. Help\n");
+    printf("3. Exit\n");
+    printf("Please enter (1-3): ");
 }
 
 void help() // 遊戲規說明函數
 {
-    printf("\t\t\tWelcome to the program\n\n\n");
+    printf("Welcome to the program\n");
     printf("Game Instructions: Enter a four-digit number, after entering, a hint of XAYB will appear.\n");
     printf("X indicates how many digits are correct and in the correct position.\n");
     printf("Y indicates how many digits are correct but in the wrong position.\n");
-    printf("Press any key to continue............\n");
+    printf("Press 1 to continue the game: ");
 }
 
-void game(int a[]) // 比較用戶輸入數和隨機數，並產生提示訊息
-{
-    int b[4] = {0};
-    int i = 0, j = 0;
+void game(int randNum[]) // 比較用戶輸入數和隨機數，並產生提示訊息
+{   int getNum[4] = {0};
 
-    time_t start_time, end_time; // 用來記錄遊戲的開始和結束時間
-    time(&start_time); // 記錄開始時間
+    creatNUM(randNum);
 
-    while (1)
-    {
-        // 生成4個隨機數字
-        do
-        {
-            for (i = 0; i < 4; i++)
-                a[i] = rand() % 10;
-            same(a);
-        } while (u); // 此處使用全局變量u
-        if (a[0] != 0)
-            break;
-    }
-
-    int k = 1, A = 0, B = 0, N = 8, pick = 0;
-    printf("Please input your guess of the four digits\n");
-    while (A != 4 && k <= N)
-    {
+    int tryCase = 1, A = 0, B = 0, total = 8, pick = 0;
+    printf("Please input your guess of the four digits:\n");
+    while (A != 4 && tryCase <= total){
         A = 0, B = 0;
-        printf("Attempt %d: ", k);
+
         scanf("%d", &pick); // 玩家從鍵盤輸入數據
-        while (pick < 999 || pick > 10000)
-        {
+        while (pick < 0 || pick > 9999){
             printf("Invalid input!\n");
             scanf("%d", &pick);
         }
-        for (i = 3; i >= 0; i--)
-        {
-            b[i] = pick % 10;
+        for (int i = 3; i >= 0; i--){
+            getNum[i] = pick % 10;
             pick = pick / 10;
         }
         // 比較對應位置上的數字
-        for (i = 0; i < 4; i++)
-            for (j = 0; j < 4; j++)
-                if (a[i] == b[j])
-                    if (i == j)
-                        A++;
-                    else
-                        B++;
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                if (randNum[i] == getNum[j]){
+                    if (i == j) A++;
+                    else B++;
+                }
+            }
+        }
         printf("%dA%dB\n", A, B);
-        if (A == 4)
-        {
+        progress_bar(tryCase, total);
+        printf("\n");
+        if (A == 4){
             printf("Congratulations, game successful!\n");
         }
-        k++;
+        tryCase++;
     }
-    if (A != 4 && k > 8)
-    {
+    if (A != 4 && tryCase > 8){
         puts("");
-        printf("Game failed!\n\n");
+        printf("Game failed!\n");
         printf("The correct answer is: ");
-        for (i = 0; i < 4; i++)
-            printf("%d", a[i]);
-        printf("\n");
+        for (int i = 0; i < 4; i++)
+            printf("%d", randNum[i]);
+        printf("\n\n");
     }
-
-    // 記錄遊戲結束時間
-    time(&end_time);
-
-    // 計算所用時間並顯示
-    record_time(start_time, end_time); // 顯示花費時間
-
-    printf("Enter 1 to continue the game, enter 2 to return to the menu, enter 0 to exit the game: ");
-    while (1)
-    {
+    printf("Enter 1 to continue the game, enter 0 to exit the game: ");
+    while (1){
         scanf("%d", &pick);
-        if (pick == 2)
-        {
-            system("cls");
-            menu();
-            int select = 0;
-            while (select != 3)
-            {
-                switch (select)
-                {
-                case 1: { system("cls"); game(a); break; }
-                case 2: { system("cls"); help(); _getch(); system("cls"); menu(); break; }
-                default: printf("Invalid input, please re-enter!"); break;
-                }
-                scanf("%d", &select);
-            }
+        if (pick == 1) game(randNum);
+        else if (pick == 0){
+            printf("Thanks for playing!\n");
             exit(0);
         }
-        else if (pick == 1)
-        {
-            system("cls");
-            game(a);
-        }
-        else if (pick == 0)
-            exit(0);
-        else
-            printf("Invalid input, please re-enter!\n");
+        else printf("Invalid input! Please enter (1-3): ");
     }
 }
-
-int same(int num[]) // 判斷產生的隨機數各個位置上有沒有重複的函數
+void creatNUM(int randNum[])
 {
-    u = 0; // 注意u是全局變量
+    while (1)
+    {// 生成4個隨機數字
+        do{
+            for (int i = 0; i < 4; i++)
+                randNum[i] = rand() % 10;
+            same(randNum);
+        } while (u); //u=1時，重新生成
+        if (randNum[0] != 0)
+            break;
+    }
+}
+int same(int num[]) // 判斷產生的隨機數各個位置上有沒有重複的函數
+{   u = 0;
     for (int i = 0; i < 3; i++)
         for (int j = i + 1; j < 4; j++)
             if (num[i] == num[j])
@@ -163,9 +128,22 @@ int same(int num[]) // 判斷產生的隨機數各個位置上有沒有重複的
     return u;
 }
 
-// 顯示遊戲花費的時間
-void record_time(time_t start_time, time_t end_time)
+// 顯示進度條函數
+void progress_bar(int current, int total)
 {
-    double time_taken = difftime(end_time, start_time); // 計算時間差
-    printf("You took %.2f seconds to complete the game.\n\n", time_taken); // 顯示所用時間
+    int bar_width = 50; // 進度條總寬度
+    int pos = (current * bar_width) / total;
+
+    printf("[");
+    for (int i = 0; i < bar_width; i++) {
+        if (i < pos - 1)  // 填滿的部分
+            printf("=");
+        else if (i == pos - 1 && current < total)
+            printf(">");
+        else if (i == bar_width - 1 && current == total)
+            printf(">");
+        else  // 剩餘空格
+            printf(" ");
+    }
+    printf("] %d chances left\n", 8 - current);
 }
